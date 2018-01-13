a4ac1e5432e5c53dba2a071c31b8b5bf1c04ccef
Andy <andytenholder@gmail.com>
2018-01-13 10:06:14 -0800

    Add project files.

---
+++ TwitterWebMVCv2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TwitterWebMVCv2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
